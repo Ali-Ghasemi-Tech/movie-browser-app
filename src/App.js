f48e960cb13843967bd465379d6f1fb45bbe0bee
TLB30 <thelastblade30@gmail.com>
2023-08-21 16:12:20 +0330

    add full movie search and rendering for all search results

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const API_URL = 'http://www.omdbapi.com?apikey=46cefbd4';
 function App() {
 
   const [movies ,setMovies] = useState([]);
+  const [searchTerm , setSearchTerm] =useState('');
 
   const searchMovie = async (title) =>{
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -37,32 +38,45 @@ function App() {
 
   return (
     <>
-      <body className='bg-gray-900 flex flex-col items-center pt-32'>
+      <body className='bg-gray-900 flex flex-col items-center pt-32 min-h-screen'>
           <h1 className='text-6xl bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-purple-600 mb-10'>Movie Land
           </h1>
 
         <div className='flex justify-between border border-indigo-500 rounded-full pr-5 w-[80%] h-[60px] pl-6 shadow-lg shadow-gray-700' >
           <input className='text-gray-400 bg-transparent text-xl w-full h-full focus:outline-none' type="text"
           placeholder='write your movie title here'
-          onChange={() => {}}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
           />
 
           <img className='py-3 hover:cursor-pointer'
           src={SearchIcon}
           alt="Search"
-          onClick={() => {}}
+          onClick={() => {
+            searchMovie(searchTerm);
+          }}
             />
         </div>
         
-        <div className='flex flex-wrap justify-center w-[85%] py-10 gap-14 '>
-          <MovieCard movie1={ movie1 } />
-          <MovieCard movie1={ movie1 } />
-          <MovieCard movie1={ movie1 } />
-          <MovieCard movie1={ movie1 } />
-          <MovieCard movie1={ movie1 } />
-          <MovieCard movie1={ movie1 } />
-          <MovieCard movie1={ movie1 } />
-        </div>
+        {
+          movies?.length > 0 
+          ? (
+            <div className='flex flex-wrap justify-center w-[85%] py-10 gap-14 '>
+            {
+              movies.map((movie) => {
+                return <MovieCard movie = {movie} />
+              })
+            }
+            </div>
+          ) :
+          <div className='flex justify-center items-center w-full h-full mt-20 text-2xl text-indigo-300'>
+            <h2>no movies found</h2>
+          </div>
+        }
+
+        
+        
           
       </body>
       
